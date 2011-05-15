0b2be832213973a1aab833caea1129fa2cb29b94
Laurent Etiemble <laurent.etiemble@monobjc.net>
2011-05-15 15:06:36 +0200

    Add new utility method.

--- Monobjc.Tools/Utilities/FileProvider.cs
+++ Monobjc.Tools/Utilities/FileProvider.cs
@@ -23,13 +23,8 @@ namespace Monobjc.Tools.Utilities
 {
     public static class FileProvider
     {
-        public static void CopyFile(MacOSVersion version, String name, String destination)
-        {
-            CopyFile(version, name, destination, null);
-        }
-
-        public static void CopyFile(MacOSVersion version, String name, String destination, String permissions)
-        {
+		public static String GetPath(MacOSVersion version, String name)
+		{
             // Set the base path
             String basedir = "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono";
             String dir = null;
@@ -45,9 +40,19 @@ namespace Monobjc.Tools.Utilities
                 default:
                     throw new NotSupportedException("Unsupported version of Mac OS X");
             }
+			
+            return Path.Combine(dir, name);
+		}
+		
+        public static void CopyFile(MacOSVersion version, String name, String destination)
+        {
+            CopyFile(version, name, destination, null);
+        }
 
+        public static void CopyFile(MacOSVersion version, String name, String destination, String permissions)
+        {
             // Copy the file
-            String file = Path.Combine(dir, name);
+            String file = GetPath(version, name);
             File.Copy(file, destination, true);
 
             if (!String.IsNullOrEmpty(permissions))
