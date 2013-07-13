c30926f6a35fe3399b4c7f354614d5ccb623becd
Laurent Etiemble <laurent.etiemble@monobjc.net>
2013-07-13 20:54:25 +0200

    Re-introduce block API everywhere.

--- Monobjc.Generator/Model/Entities/BaseEntity.Patching.cs
+++ Monobjc.Generator/Model/Entities/BaseEntity.Patching.cs
@@ -14,11 +14,16 @@ namespace Monobjc.Tools.Generator.Model
 		
 		public static void Change<T> (T entity, String statement) where T : BaseEntity
 		{
-			ParameterExpression p = Expression.Parameter (typeof(T), "entity");
-			LambdaExpression expr = System.Linq.Dynamic.DynamicExpression.ParseAction<T> (new []{ p }, "entity." + statement, null);
-			//Console.WriteLine ("Patching '" + entity.Name + "' with '" + expr + "'");
-			Action<T> action = (Action<T>)expr.Compile ();
-			action (entity);
+            ParameterExpression p = Expression.Parameter (typeof(T), "entity");
+            LambdaExpression expr = System.Linq.Dynamic.DynamicExpression.ParseAction<T> (new []{ p }, "entity." + statement, null);
+
+            try {
+                //Console.WriteLine ("Patching '" + entity.Name + "' with '" + expr + "'");
+                Action<T> action = (Action<T>)expr.Compile ();
+                action (entity);
+            } catch(Exception e) {
+                Console.WriteLine ("Error while patching '" + entity.Name + "' with '" + expr + "'");
+            }
 		}
 	}
 }
